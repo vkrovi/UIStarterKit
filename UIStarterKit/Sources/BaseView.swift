92674dd251c84df14fe8bd7f452ba690e3ed052a
Venkat Krovi <v.r.krovi@gmail.com>
2020-07-28 16:29:54 -0400

    backup

--- UIStarterKit/Sources/BaseView.swift
+++ UIStarterKit/Sources/BaseView.swift
@@ -114,3 +114,25 @@ extension UITableView {
         return tableView
     }
 }
+
+public typealias BaseTableViewCell = BaseTableViewCellClass & BaseViewProtocols
+
+open class BaseTableViewCellClass: UITableViewCell {
+    
+    public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        guard let delegate = self as? BaseViewProtocols else { return }
+        delegate.setupView()
+        delegate.setupLayout()
+    }
+
+}
